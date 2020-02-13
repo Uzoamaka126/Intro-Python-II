d2cc6ffa2b3d57153b7e6c529bc30c08c2ed61f6
Uzoamaka126 <vanessapeters126@gmail.com>
2020-02-13 14:40:58 +0100

    Add user direction

--- src/adv.py
+++ src/adv.py
@@ -4,21 +4,21 @@ from room import Room
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", "sheath"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", "sword"),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", "bread"),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", "torch"),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", "water"),
 }
 
 
@@ -36,32 +36,44 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 destinations = ["n", "s", "w", "e"]
+items = ["sword", "sheath", "water", "bread", "flag"]
 
 from player import Player
 # create a starting room
 starting_room = room['outside']
 direction = input("enter direction")
+
 # Make a new player object that is currently in the 'outside' room.
-default_player = Player('one', starting_room)
+default_player = Player('one', starting_room, 'sword')
+
 # Write a loop that:
 while True:
+
 # * Prints the current room name
 
 # * Prints the current description (the textwrap module might be useful here).
-    print(f"You went: {direction}")
 # * Waits for user input and decides what to do.
-def get_user_input():
-    user = input("> Where do you want to go now:")
-    user = user.split(" ")
-    print(user)
-    return user
+    def get_user_input():
+        user = input("> Where do you want to go now:")
+        user = user.split(" ")
+        print(user)
+        return user
 # If the user enters a cardinal direction, attempt to move to the room there.
+    if(user)
+
 # Print an error message if the movement isn't allowed.
 
-def room_direction(room):
+def set_room_details(room):
     print("Error")
+    print(f"You went: {direction}")
     print(f"You are currently in: {default_player.room.name}")
     print(room.description)
+    # Make the room be able to hold multiple items
+    if len(room.inventory == 0):
+        print("Sorry, there are no items!")
+    else:
+        for item in room.inventories:
+            print(f"Here, have this item: {item}")
     
 
 # If the user enters "q", quit the game.
--- src/room.py
+++ src/room.py
@@ -1,14 +1,19 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.''
-
 class Room:
-    def __init__(self, name, age):
+    def __init__(self, name, description, inventory):
         self.name = name
         self.description = description
         self.n_to = None
         self.s_to = None
         self.e_to = None
         self.w_to = None
+        self.inventory = inventory
+
+    def add_items(self, item):
+        self.inventory.append(item)
+
+
 
         
 
